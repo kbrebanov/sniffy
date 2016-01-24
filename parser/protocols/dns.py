e0327552ecbdb080fa9f67e15001ebaa99a1dd3f
Kevin Brebanov <kevin.brebanov@gmail.com>
2016-01-24 11:12:52 -0500

    Use dnspython to implement DNS parsing

--- parser/protocols/dns.py
+++ parser/protocols/dns.py
@@ -1,62 +1,120 @@
 import binascii
 
-import dnslib
-#import dnspython3
+import dns.message
 
 class DNSPacket:
     def __init__(self, data):
         self.data = data
         fields = self._parse()
         self.id = fields[0]
-        self.qr = fields[1]
-        self.opcode = fields[2]
-        self.aa = fields[3]
-        self.tc = fields[4]
-        self.rd = fields[5]
-        self.ra = fields[6]
-        self.rcode = fields[7]
-        self.total_questions = len(fields[8])
-        self.total_answer_rrs = len(fields[9])
-        self.total_authority_rrs = len(fields[10])
-        self.total_additional_rrs = len(fields[11])
-        self.questions = fields[8]
-        self.answer_rrs = fields[9]
-        self.authority_rrs = fields[10]
-        self.additional_rrs = fields[11]
+        self.flags = fields[1]
+        self.total_questions = fields[2]
+        self.total_answer_rrs = fields[3]
+        self.total_authority_rrs = fields[4]
+        self.total_additional_rrs = fields[5]
+        self.questions = fields[6]
+        self.answer_rrs = fields[7]
+        self.authority_rrs = fields[8]
+        self.additional_rrs = fields[9]
 
     def _parse(self):
-        record = dnslib.DNSRecord.parse(self.data)
-        #message = dnspython3.dns.message.from_wire(self.data)
-        record_id = 0
-        qr = record.header.get_qr()
-        opcode = record.header.get_opcode()
-        aa = record.header.get_aa()
-        tc = record.header.get_tc()
-        rd = record.header.get_rd()
-        ra = record.header.get_ra()
-        rcode = record.header.get_rcode()
+        message = dns.message.from_wire(self.data)
+        message_id = message.id
+        
+        flags = {}
+        print(message.flags)
+        if (message.flags & 32768) == 32768:
+            flags["qr"] = 1
+        else:
+            flags["qr"] = 0
+        flags["opcode"] = message.opcode()
+        if (message.flags & 1024) == 1024:
+            flags["aa"] = 1
+        else:
+            flags["aa"] = 0
+        if (message.flags & 512) == 512:
+            flags["tc"] = 1
+        else:
+            flags["tc"] = 0
+        if (message.flags & 256) == 256:
+            flags["rd"] = 1
+        else:
+            flags["rd"] = 0
+        if (message.flags & 128) == 128:
+            flags["ra"] = 1
+        else:
+            flags["ra"] = 0
+        if (message.flags & 64) == 64:
+            flags["z"] = 1
+        else:
+            flags["z"] = 0
+        if (message.flags & 32) == 32:
+            flags["ad"] = 1
+        else:
+            flags["ad"] = 0
+        if (message.flags & 16) == 16:
+            flags["cd"] = 1
+        else:
+            flags["cd"] = 0
+        flags["rcode"] = message.rcode()
+
+        total_questions = len(message.question)
+        total_answer_rrs = len(message.answer)
+        total_authority_rrs = len(message.authority)
+        total_additional_rrs = len(message.additional)
+
         questions = []
-        for q in record.questions:
-            questions.append({"name": q.get_qname(),
-                              "type": q.qtype,
-                              "class": q.qclass})
-        print(questions)
-        answer_rrs = record.rr
-        authority_rrs = record.auth
-        additional_rrs = record.ar
-        return (record_id, qr, opcode, aa, tc, rd, ra, rcode, questions,
+        for question in message.question:
+            questions.append({"name": question.name.to_text(),
+                              "type": question.rdtype,
+                              "class": question.rdclass})
+        answer_rrs = []
+        for answer in message.answer:
+            rdata_items = []
+            for item in answer.items:
+                rdata_items.append(item.to_text())
+
+            answer_rrs.append({"name": answer.name.to_text(),
+                               "type": answer.rdtype,
+                               "class": answer.rdclass,
+                               "ttl": answer.ttl,
+                               "rdata_length": len(answer.items),
+                               "rdata": rdata_items})
+
+        authority_rrs = []
+        for authority in message.authority:
+            rdata_items = []
+            for item in authority.items:
+                rdata_items.append(item.to_text())
+
+            authority_rrs.append({"name": authority.name.to_text(),
+                                  "type": authority.rdtype,
+                                  "class": authority.rdclass,
+                                  "ttl": authority.ttl,
+                                  "rdata_length": len(authority.items),
+                                  "rdata": rdata_items})
+
+        additional_rrs = []
+        for additional in message.additional:
+            rdata_items = []
+            for item in additional.items:
+                rdata_items.append(item.to_text())
+
+            additional_rrs.append({"name": additional.name.to_text(),
+                                   "type": additional.rdtype,
+                                   "class": additional.rdclass,
+                                   "ttl": additional.ttl,
+                                   "rdata_length": len(additional.items),
+                                   "rdata": rdata_items})
+
+        return (message_id, flags, total_questions, total_answer_rrs,
+                total_authority_rrs, total_additional_rrs, questions,
                 answer_rrs, authority_rrs, additional_rrs)
 
     def header(self):
         h = {
           "id": self.id,
-          "qr": self.qr,
-          "opcode": self.opcode,
-          "aa": self.aa,
-          "tc": self.tc,
-          "rd": self.rd,
-          "ra": self.ra,
-          "rcode": self.rcode,
+          "flags": self.flags,
           "total_questions": self.total_questions,
           "total_answer_rrs": self.total_answer_rrs,
           "total_authority_rrs": self.total_authority_rrs,
